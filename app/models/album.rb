502c747c54cd699eab35601b569b5fb532d833ba
Mat Harden <matharden@gmail.com>
2013-08-29 22:05:03 +0100

    Added validation to attributes

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,8 @@
 class Album < ActiveRecord::Base
+  validates :title, presence: true
+  validates :artist, presence: true
+  validates :host, presence: true
+  validates :played_on, presence: true
+
   default_scope :order => 'played_on DESC'
 end
