0a9d7896604e31331a02381b563f700b3567c7e6
Mat Harden <matharden@gmail.com>
2013-10-20 22:19:05 +0100

    Twitter user authentication
    https://trello.com/c/chsLo6eg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130718130147) do
+ActiveRecord::Schema.define(version: 20131020161850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20130718130147) do
     t.string   "artist"
     t.string   "host"
     t.datetime "played_on"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
