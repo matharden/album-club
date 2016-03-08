4f0ff7e54be7192b490a2806682b73d5a8366f4e
Mat Harden <matharden@gmail.com>
2016-03-08 23:41:43 +0000

    Add BrowserSync to Gulp builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
+var browserSync = require('browser-sync');
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
 
-gulp.task('default', function () {
+gulp.task('sass', function (){
   return gulp.src('./gulp/assets/stylesheets/app.sass')
     .pipe( sourcemaps.init() )
     .pipe( sass().on('error', sass.logError) )
     .pipe( postcss([ require('autoprefixer'), require('precss') ]) )
     .pipe( sourcemaps.write('.') )
-    .pipe( gulp.dest('public/assets/stylesheets/') );
+    .pipe( gulp.dest('public/assets/stylesheets/') )
+    .pipe( browserSync.reload({stream:true}) );
 });
+
+gulp.task('browser-sync', function(){
+  browserSync({
+    proxy: 'localhost:3000',
+    files: ['./app/views/**']
+  });
+});
+
+gulp.task('watch', ['browser-sync'], function(callback){
+  watch('./gulp/assets/stylesheets/**/*.sass', function(){
+    gulp.start('sass');
+  });
+});
+
+gulp.task('default', ['sass', 'watch']);
