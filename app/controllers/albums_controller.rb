8d1f43987cf7a15a5c5d50465d12457fea19ea4f
Mat Harden <matharden@gmail.com>
2016-09-15 23:54:51 +0100

    Add host filter
    Add filter
    
    Fix webkit

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,6 +5,7 @@ class AlbumsController < ApplicationController
   # GET /albums.json
   def index
     @albums = Album.all
+    @hosts = Album.order(:host).pluck(:host).uniq
   end
 
   # GET /albums/1
